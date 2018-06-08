030f8ee8ae535be7074eae1c34f38c9c2148c7b9
Uriy Kurnosov <teisintai@gmail.com>
2018-06-08 13:23:39 +0300

    360 panoramic screenshot added (P button)

---
+++ Assets/Scripts/RenderTest.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class RenderTest : MonoBehaviour
+{
+	public int imageWidth = 4096;
+	public bool saveAsJPEG = true;
+
+	void Update()
+	{
+		if( Input.GetKeyDown( KeyCode.P ) )
+		{
+			byte[] bytes = I360Render.Capture( imageWidth, saveAsJPEG );
+			if( bytes != null )
+			{
+				string path = Path.Combine( Application.persistentDataPath, "360render" + ( saveAsJPEG ? ".jpeg" : ".png" ) );
+				File.WriteAllBytes( path, bytes );
+				Debug.Log( "360 render saved to " + path );
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/SlowRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowRotation : MonoBehaviour {
+
+	public float rotationSpeed = 0.5f;
+
+	// Use this for initialization
+	void Awake () {
+		StartCoroutine ("Rotation");
+	}
+
+	private IEnumerator Rotation(){
+		while (true) {
+			transform.Rotate (0, Mathf.LerpUnclamped(0.0f,1.0f,rotationSpeed/(Time.deltaTime+0.1f)), 0);
+			yield return null;
+		}
+	}
+}
